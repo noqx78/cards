c88dafad0b94c061bb66e2edf58e34037758ddf9
Noel Merz <noel_merz@sluz.ch>
2026-04-24 11:17:44 +0200

    zwischenstand

--- cards.ts
+++ cards.ts
@@ -1,3 +1,42 @@
 export function hello(): string {
   return "world";
 }
+
+class Card {
+  color: number;
+  number: Number;
+
+  beats(other: Card): boolean {
+
+    if (this.number === other.number) {
+      return false;
+    }
+
+  }
+}
+
+enum colorValues {
+  Ass = 14,
+  König = 13,
+  Ober = 12,
+  Under = 11,
+  Banner = 10
+}
+
+enum color {
+  Rosen = 3,
+  Schellen = 2,
+  Schilten = 2,
+  Eicheln = 1,
+}
+
+
+const card1 = new Card();
+card1.number = colorValues.Ass;
+card1.color = color.Rosen
+
+const card2 = new Card();
+card1.number = colorValues.Banner;
+card1.color = color.Schellen
+
+card1.beats(card2)
